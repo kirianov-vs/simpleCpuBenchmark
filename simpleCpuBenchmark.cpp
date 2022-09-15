ab23d8e72de26196e58bda1dde11e6710f1a0757
Vladimir K <ip-kiryanov@yandex.ru>
2022-09-15 20:20:18 +0200

    replaced thread creating procedure, now creating inside loop

--- simpleCpuBenchmark.cpp
+++ simpleCpuBenchmark.cpp
@@ -13,73 +13,19 @@ void pi(int n)
     }
     double a = 4.0 * sum;
 }
+
 void benchmark()
 {
+    std::thread myThreads[32];
     int piAccuracy = 25000000;
-    std::thread thread_obj1(pi, piAccuracy);
-    std::thread thread_obj2(pi, piAccuracy);
-    std::thread thread_obj3(pi, piAccuracy);
-    std::thread thread_obj4(pi, piAccuracy);
-    std::thread thread_obj5(pi, piAccuracy);
-    std::thread thread_obj6(pi, piAccuracy);
-    std::thread thread_obj7(pi, piAccuracy);
-    std::thread thread_obj8(pi, piAccuracy);
-    std::thread thread_obj9(pi, piAccuracy);
-    std::thread thread_obj10(pi, piAccuracy);
-    std::thread thread_obj11(pi, piAccuracy);
-    std::thread thread_obj12(pi, piAccuracy);
-    std::thread thread_obj13(pi, piAccuracy);
-    std::thread thread_obj14(pi, piAccuracy);
-    std::thread thread_obj15(pi, piAccuracy);
-    std::thread thread_obj16(pi, piAccuracy);
-    std::thread thread_obj17(pi, piAccuracy);
-    std::thread thread_obj18(pi, piAccuracy);
-    std::thread thread_obj19(pi, piAccuracy);
-    std::thread thread_obj20(pi, piAccuracy);
-    std::thread thread_obj21(pi, piAccuracy);
-    std::thread thread_obj22(pi, piAccuracy);
-    std::thread thread_obj23(pi, piAccuracy);
-    std::thread thread_obj24(pi, piAccuracy);
-    std::thread thread_obj25(pi, piAccuracy);
-    std::thread thread_obj26(pi, piAccuracy);
-    std::thread thread_obj27(pi, piAccuracy);
-    std::thread thread_obj28(pi, piAccuracy);
-    std::thread thread_obj29(pi, piAccuracy);
-    std::thread thread_obj30(pi, piAccuracy);
-    std::thread thread_obj31(pi, piAccuracy);
-    std::thread thread_obj32(pi, piAccuracy);
-    thread_obj1.join();
-    thread_obj2.join();
-    thread_obj3.join();
-    thread_obj4.join();
-    thread_obj5.join();
-    thread_obj6.join();
-    thread_obj7.join();
-    thread_obj8.join();
-    thread_obj9.join();
-    thread_obj10.join();
-    thread_obj11.join();
-    thread_obj12.join();
-    thread_obj13.join();
-    thread_obj14.join();
-    thread_obj15.join();
-    thread_obj16.join();
-    thread_obj17.join();
-    thread_obj18.join();
-    thread_obj19.join();
-    thread_obj20.join();
-    thread_obj21.join();
-    thread_obj22.join();
-    thread_obj23.join();
-    thread_obj24.join();
-    thread_obj25.join();
-    thread_obj26.join();
-    thread_obj27.join();
-    thread_obj28.join();
-    thread_obj29.join();
-    thread_obj30.join();
-    thread_obj31.join();
-    thread_obj32.join();
+    for (int i = 0; i < 32; i++)
+    {
+        myThreads[i] = std::thread(pi, piAccuracy);
+    }
+    for (int i = 0; i < 32; i++)
+    {
+        myThreads[i].join();
+    }
 }
 
 int main()
